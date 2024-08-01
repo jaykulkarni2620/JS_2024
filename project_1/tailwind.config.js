adfcac2ae8c44fed0f4afc9f1abe41bf1a1db681
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-08-01 20:42:55 +0530

    Weather App

---
+++ project_1/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './*.html',   // Include all HTML files in the root
+    './*.js',     // Include all JS files in the root
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
